d8c240f53235e08203532cbd24b4bd8e9fa06acf
Skye Qiu <skyeq@wirelessprv-10-192-234-6.near.illinois.edu>
2023-04-27 15:47:55 -0500

    ?

--- Honors/src/lib.rs
+++ Honors/src/lib.rs
@@ -1,1 +1,13 @@
 use docx_rs::*;
+
+pub fn merge_docx(input: String) -> Result<(), DocxError> {
+    let path = std::path::Path::new("./output.docx");
+    let file = std::fs::File::create(&path).unwrap();
+
+    Docx::new()
+        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(input)))
+        .build()
+        .pack(file)?;
+
+    Ok(())
+}
